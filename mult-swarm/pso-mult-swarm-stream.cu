aa2435985339f4dc5f7ca54d032430a0caa8dae0
Eduardo Sampaio <edwardviana@gmail.com>
2022-01-13 19:46:08 -0300

    ajuste mult stream

--- mult-swarm/pso-mult-swarm-stream.cu
+++ mult-swarm/pso-mult-swarm-stream.cu
@@ -40,7 +40,7 @@ const double MaxValue = 1.7976931348623157E+308;
 const int num_dimensions = 2;
 const int num_particle = 4096;
 const int num_swarms = 16;
-const int THREAD_PER_BLOCK = 256;
+const int THREAD_PER_BLOCK = 128;
 const int BLOCKS = num_particle / THREAD_PER_BLOCK;
 const int MAX_ITER = 30;
 
@@ -69,54 +69,11 @@ __device__ double sphere(Particle *x)
     return total;
 }
 
-__global__ void update_gbest(Particle *swarm, double *pos_best_s, double *err_best_s)
-{
-
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-
-    if (swarm[i].err_i < *err_best_s || *err_best_s == -1)
-    {
-        pos_best_s[0] = swarm[i].position_i[0];
-        pos_best_s[1] = swarm[i].position_i[1];
-        *err_best_s = swarm[i].err_i;
-        // printf("Solution Swarm:%d [x:%.20f, y:% .20f]\n", i, swarm[i].position_i[0], swarm[i].position_i[1]);
-    }
-}
-
-__global__ void update_position_velocity(Particle *swarm, double *bounds, double *pos_best_g, double *numbers)
-{
-
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    double w = 0.9;
-    int c1 = 1;
-    int c2 = 2;
-
-    for (int j = 0; j < num_dimensions; j++)
-    {
-        swarm[i].position_i[j] = swarm[i].position_i[j] + swarm[i].velocity_i[j];
-        if (swarm[i].position_i[j] > bounds[j * num_dimensions + 1])
-        {
-            swarm[i].position_i[j] = bounds[j * num_dimensions + 1];
-        }
-
-        if (swarm[i].position_i[j] < bounds[j * num_dimensions + 0])
-        {
-            swarm[i].position_i[j] = bounds[j * num_dimensions + 0];
-        }
-
-        double r1 = numbers[i];
-        double r2 = numbers[i + j];
-        double vel_cognitive = c1 * r1 * (swarm[i].pos_best_i[j] - swarm[i].position_i[j]);
-        double vel_social = c2 * r2 * (pos_best_g[j] - swarm[i].position_i[j]);
-        swarm[i].velocity_i[j] = w * swarm[i].velocity_i[j] + vel_cognitive + vel_social;
-    }
-}
-
 __global__ void update_AllParticle_position_velocity(Swarm *swarms, double *bounds, double *pos_best_g, double *numbers, int num_swarms, int num_particle, int num_dimensions)
 {
 
     int i = threadIdx.x + blockIdx.x * blockDim.x;
-    double w = 0.9;
+    double w = 0.4;
     int c1 = 1;
     int c2 = 2;
     int c3 = 1;
@@ -136,7 +93,7 @@ __global__ void update_AllParticle_position_velocity(Swarm *swarms, double *boun
                 double vel_sbest = c3 * r3 * (swarms[k].pos_best_s[j] - swarms[k].swarm[i].position_i[j]);
                 swarms[k].swarm[i].velocity_i[j] = w * swarms[k].swarm[i].velocity_i[j] + vel_cognitive + vel_social + vel_sbest;
 
-                swarms[k].swarm[i].position_i[j] = swarms[k].swarm[i].position_i[j] + swarms[k].swarm[i].velocity_i[j];
+                swarms[k].swarm[j].position_i[i] = swarms[k].swarm[j].position_i[i] + swarms[k].swarm[j].velocity_i[i];
 
                 if (swarms[k].swarm[i].position_i[j] > bounds[j * num_dimensions + 1])
                 {
@@ -149,24 +106,6 @@ __global__ void update_AllParticle_position_velocity(Swarm *swarms, double *boun
                 }
             }
         }
-    }
-}
-
-__global__ void calculate_fitness(Particle *swarm)
-{
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    double err = sphere(&swarm[i]);
-    swarm[i].err_i = err;
-}
-
-__global__ void evaluate_update_pbest(Particle *swarm)
-{
-
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    if (swarm[i].err_i < swarm[i].err_best_i)
-    {
-        swarm[i].pos_best_i = swarm[i].position_i;
-        swarm[i].err_best_i = swarm[i].err_i;
     }
 }
 
@@ -190,7 +129,6 @@ __global__ void init_particle(Swarm *swarms, double *x0, double *numbers, int nu
         {
             swarms[k].swarm[i].position_i[j] = x0[j];
             swarms[k].swarm[i].velocity_i[j] = numbers[i + j + k];
-            // printf("Swarm: %d Thread: %d Position: %f Velocity %f \n", k, i, x0[j], numbers[i + j + k]);
         }
     }
 }
@@ -234,6 +172,7 @@ void initialize_gpu_memory()
     h_swarms = init_host_swarm(num_swarms, num_particle, num_dimensions);
     cudaMalloc(&pos_best_g, sizeof(double) * num_dimensions);
     cudaMalloc(&err_best_g, sizeof(double));
+    cudaMalloc(&swarm, sizeof(Particle) * num_particle);
     cudaMalloc(&swarms, sizeof(Swarm) * num_swarms);
     cudaMalloc(&BOUNDS, sizeof(double) * num_dimensions * num_dimensions);
     cudaMalloc(&initial, sizeof(double) * num_dimensions);
@@ -265,21 +204,63 @@ void initialize_gpu_memory()
     }
 
     HANDLE_ERROR(cudaMemcpyAsync(swarms, h_swarms, sizeof(Swarm) * num_swarms, cudaMemcpyHostToDevice));
+}
 
-    init_particle<<<BLOCKS, THREAD_PER_BLOCK>>>(swarms, initial, numbersRand, num_swarms);
+__global__ void pso(Particle *swarm, double *pos_best_s, double *err_best_s, double *bounds, double *numbers)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    swarm[i].err_i = sphere(&swarm[i]);
+
+    if (swarm[i].err_i < swarm[i].err_best_i)
+    {
+        swarm[i].pos_best_i = swarm[i].position_i;
+        swarm[i].err_best_i = swarm[i].err_i;
+    }
+
+    if (swarm[i].err_i < *err_best_s || *err_best_s == -1)
+    {
+        pos_best_s[0] = swarm[i].position_i[0];
+        pos_best_s[1] = swarm[i].position_i[1];
+        *err_best_s = swarm[i].err_i;
+    }
+
+    double w = 0.9;
+    int c1 = 1;
+    int c2 = 2;
+
+    for (int j = 0; j < num_dimensions; j++)
+    {
+        swarm[i].position_i[j] = swarm[i].position_i[j] + swarm[i].velocity_i[j];
+        if (swarm[i].position_i[j] > bounds[j * num_dimensions + 1])
+        {
+            swarm[i].position_i[j] = bounds[j * num_dimensions + 1];
+        }
+
+        if (swarm[i].position_i[j] < bounds[j * num_dimensions + 0])
+        {
+            swarm[i].position_i[j] = bounds[j * num_dimensions + 0];
+        }
+
+        double r1 = numbers[i];
+        double r2 = numbers[i + j];
+        double vel_cognitive = c1 * r1 * (swarm[i].pos_best_i[j] - swarm[i].position_i[j]);
+        double vel_social = c2 * r2 * (pos_best_s[j] - swarm[i].position_i[j]);
+        swarm[i].velocity_i[j] = w * swarm[i].velocity_i[j] + vel_cognitive + vel_social;
+    }
 }
 
 int main()
 {
-    cudaDeviceReset();
     float time = 0.0;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
-    initialize_gpu_memory();
-    cudaStream_t stream[num_swarms];
 
+    initialize_gpu_memory();
+    init_particle<<<BLOCKS, THREAD_PER_BLOCK>>>(swarms, initial, numbersRand, num_swarms);
+    cudaStream_t stream[num_swarms];
     for (int i = 0.; i < num_swarms; i++)
     {
         double h_pos_best_s[2];
@@ -288,39 +269,33 @@ int main()
         HANDLE_ERROR(cudaStreamCreate(&stream[i]));
         while (k < MAX_ITER)
         {
-            calculate_fitness<<<BLOCKS, THREAD_PER_BLOCK, 0, stream[i]>>>(h_swarms[i].swarm);
-            evaluate_update_pbest<<<BLOCKS, THREAD_PER_BLOCK, 0, stream[i]>>>(h_swarms[i].swarm);
-            update_gbest<<<BLOCKS, THREAD_PER_BLOCK, 0, stream[i]>>>(h_swarms[i].swarm, h_swarms[i].pos_best_s, h_swarms[i].err_best_s);
-            update_position_velocity<<<BLOCKS, THREAD_PER_BLOCK, 0, stream[i]>>>(h_swarms[i].swarm, BOUNDS, h_swarms[i].pos_best_s, numbersRand);
+            pso<<<BLOCKS, THREAD_PER_BLOCK, 0, stream[i]>>>(h_swarms[i].swarm, h_swarms[i].pos_best_s, h_swarms[i].err_best_s, BOUNDS, numbersRand);
+            HANDLE_ERROR(cudaMemcpyAsync(h_pos_best_s, h_swarms[i].pos_best_s, sizeof(double) * num_dimensions, cudaMemcpyDeviceToHost));
+            HANDLE_ERROR(cudaMemcpyAsync(&h_err_best_s, h_swarms[i].err_best_s, sizeof(double), cudaMemcpyDeviceToHost));
+
+            if (h_pos_best_s[0] < h_pos_best_g[0] && h_pos_best_s[1] < h_pos_best_g[1])
+            {
+                h_pos_best_g[0] = h_pos_best_s[0];
+                h_pos_best_g[1] = h_pos_best_s[1];
+                h_err_best_g = h_err_best_s;
+            }
             k++;
         }
-        cudaDeviceSynchronize();
-        HANDLE_ERROR(cudaMemcpyAsync(h_pos_best_s, h_swarms[i].pos_best_s, sizeof(double) * num_dimensions, cudaMemcpyDeviceToHost));
-        HANDLE_ERROR(cudaMemcpyAsync(&h_err_best_s, h_swarms[i].err_best_s, sizeof(double), cudaMemcpyDeviceToHost));
-
-        printf("Solution Swarm:%d [x:%.20f, y:% .20f] error: % .20f\n", i, h_pos_best_s[0], h_pos_best_s[1], h_err_best_s);
-
-        if (h_pos_best_s[0] < h_pos_best_g[0] && h_pos_best_s[1] < h_pos_best_g[1])
-        {
-            h_pos_best_g[0] = h_pos_best_s[0];
-            h_pos_best_g[1] = h_pos_best_s[1];
-            h_err_best_g = h_err_best_s;
-        }
     }
-    cudaDeviceSynchronize();
+
     update_AllParticle_position_velocity<<<BLOCKS, THREAD_PER_BLOCK>>>(h_swarms, BOUNDS, pos_best_g, numbersRand, num_swarms, num_particle, num_dimensions);
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
 
     cudaEventElapsedTime(&time, start, stop);
     printf("Final Solution: [x:%.20f, y:% .20f] error: % .20f\n", h_pos_best_g[0], h_pos_best_g[1], h_err_best_g);
-    printf("Time elapsed on GPU: %3.2f ms.\n", time);
+    printf("Time elapsed on CPU: %3.2f ms.\n", time);
 
     for (int i = 0; i < num_swarms; i++)
     {
         HANDLE_ERROR(cudaStreamDestroy(stream[i]));
     }
- 
+
     cudaError_t err = cudaGetLastError();
     HANDLE_ERROR(err);
     return 0;
